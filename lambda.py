d88736f0cc90b9ab434189bded7ab6892376ab09
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-04-25 21:24:46 +0300

    chore - lambda

--- lambda.py
+++ lambda.py
@@ -3,3 +3,37 @@ squared = lambda num : num*num
 print(squared(2))
 
 addTwo = lambda num : num+2
+
+
+def funcBuilder(x):
+    return lambda num : num+x
+
+addTen = funcBuilder(10)
+addTwenty = funcBuilder(20)
+
+
+print(addTen(7))
+print(addTwenty(7))
+
+
+
+numbers = [3,7,12,18,20,21]
+
+squared_nums = map(lambda num : num*num, numbers)
+
+print(squared_nums,list(squared_nums))
+
+
+lambda num : num%2 !=0
+
+odd_nums = filter(lambda num : num%2 !=0, numbers)
+
+print(list(odd_nums))
+
+from functools import reduce
+
+lambda acc, curr : acc + curr
+
+sum_nums = reduce(lambda acc, curr : acc+curr, numbers)
+
+print(sum_nums)
